281fde2c97237c9a1f0800597b64cf51984c1749
AshleyPinto <23h05.ashley@sjec.ac.in>
2024-08-30 18:43:27 +0530

    gave indentation

--- week-01/solution.cpp
+++ week-01/solution.cpp
@@ -1,24 +1,15 @@
-
-
 #include<iostream>
 int main() {
-int a, b;
-std::cin >> a >> b;
-for(int i = a; i <= b; i++) {
+	int a, b;
+	std::cin >> a >> b;
+	for(int i = a; i <= b; i++) {
+		if (i % 3==0) {
+			std::cout << "foo\n"; 
+		} else if (i % 2 == 0 && i % 3!=0) {
 
-if(i % 3==0)
-{
-
-std::cout << "foo\n"; 
-}
-else if(i % 2 == 0 && i % 3!=0)
-{
-
-std::cout << "bar\n";
-}
-else
-{
-std::cout << "baz\n";
-}
-}
+			std::cout << "bar\n";
+		} else {
+			std::cout << "baz\n";
+		}
+	}
 } 
