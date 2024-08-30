2360d8c1732f8975e9cbeee6af7ebd6f37dd874e
AshleyPinto <23h05.ashley@sjec.ac.in>
2024-08-30 16:55:43 +0530

    made a new code

--- week-01/solution.cpp
+++ week-01/solution.cpp
@@ -1,7 +1,23 @@
+
+
 #include<iostream>
 int main() {
 int a, b;
 std::cin >> a >> b;
 for(int i = a; i <= b; i++) {
 
+if(a % 3==0)
+{
+
+std::cout << "foo"; 
+}
+else if(a % 2 == 0)
+{
+
+std::cout << "bar
+}
+else
+{
+std::cout << "baz"
+}
 } 
