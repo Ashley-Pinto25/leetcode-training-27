02c4872ab800d9d554104c8e7fbe38044b833e3b
Ashley Cleon Pinto <148516073+Ashley-Pinto25@users.noreply.github.com>
2024-08-30 18:37:14 +0530

    Update solution.cpp

--- week-01/solution.cpp
+++ week-01/solution.cpp
@@ -6,12 +6,12 @@ int a, b;
 std::cin >> a >> b;
 for(int i = a; i <= b; i++) {
 
-if(a % 3==0)
+if(i % 3==0)
 {
 
 std::cout << "foo\n"; 
 }
-else if(a % 2 == 0 && a % 3!=0)
+else if(i % 2 == 0 && i % 3!=0)
 {
 
 std::cout << "bar\n";
